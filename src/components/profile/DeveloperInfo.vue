c484e77899c2614ec89dba41e25f91b4976093cb
whiled0s <whiled0s@yandex.ru>
2019-01-29 15:30:05 +0400

    added developer info component, less and less-loader in package.json

---
+++ src/components/profile/DeveloperInfo.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="developer-info">
+    <div 
+      class="developer-info__item"
+      v-for="(value, property) in placeholders"
+      :key="property"
+    >
+      <div class="developer-info__item-name">{{value}}</div>
+      <div class="developer-info__item-text">ИТАСУ</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DeveloperInfo",
+  // props: {
+  //   institute: {
+  //     type: String,
+  //     required: true
+  //   },
+  //   faculty: {
+  //     type: String,
+  //     required: true
+  //   },
+  //   specialization: {
+  //     type: String,
+  //     required: true
+  //   },
+  //   course: {
+  //     type: Number,
+  //     required: true
+  //   },
+  //   date: {
+  //     type: Date,
+  //     required: true
+  //   },
+  //   profileVk: {
+  //     type: String,
+  //     required: true
+  //   },
+  //   email: {
+  //     type: String,
+  //     required: true
+  //   },
+  //   phoneNumber: {
+  //     type: String,
+  //     required: true
+  //   }
+  // },
+  data() {
+    return {
+      placeholders: {
+        institute: "Институт",
+        faculty: "Кафедра",
+        specialization: "Специальность",
+        course: "Курс",
+        date: "В Лиге Разработчиков",
+        profileVk: "Профиль Вконтакте",
+        email: "E-mail",
+        phoneNumber: "Номер телефона"
+    }
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -1,7 +1,83 @@
 <template>
-  <div></div>
+  <div class="developer-page">
+    <div class="developer-confirmation-status">
+      <!-- <span style="color: #ccc;">Подтверждена почта</span> -->
+      <span style="color: #ccc;">Подтверждён администратором</span>
+      <!-- <span style="color: #ccc;">Не подтверждён</span> -->
+    </div>
+    <div
+      class="developer-photo"
+      style="background: url('https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fdeveloper.amd.com%2Fwordpress%2Fmedia%2F2012%2F09%2FDeveloperImage.png&f=1'); 
+      background-position: center center;
+      background-repeat: no-repeat; 
+      background-size: cover"
+    ></div>
+    <div class="developer-name">name</div>
+    <div class="developer-role">Backend-разработчик</div>
+
+    <div class="dividing-line"></div>
+
+    <DeveloperInfo />
+
+    <div class="dividing-line"></div>
+    
+    <h1 class="developer-page__headline headline">Портфолио</h1>
+
+    <div class="developer-projects">
+      <div class="projects-section-container">
+        <a class="projects-section__link-to-project">
+          <div class="projects-section__project">
+            <div
+              style="background: url('https://africanreporter.co.za/wp-content/uploads/sites/32/2016/03/pic.jpg';
+              background-position: center center;
+              background-repeat: no-repeat;
+              background-size: cover;"
+              class="project__background"
+            ></div>
+            <div class="project__info-container">
+              <div class="project__name"></div>
+              <div class="project__status">Cтатус:
+                <span>
+                  <!-- <span
+                    class="status__value"
+                    style="background: #795548;"                    
+                  >Запланирован</span>
+                  <span
+                    class="status__value"
+                    style="background: #1e88e5;"                    
+                  >В процессе</span>
+                  <span
+                    class="status__value"
+                    style="background: #607d8b;"                    
+                  >Заморожен</span> -->
+                  <span
+                    class="status__value"
+                    style="background: #009688;"
+                  >Завершен</span>
+                </span>
+              </div>
+              <div class="project__developer-role"><b>Роль разработчика:</b></div>
+              <div class="project__developer-role-text">Backend</div>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import DeveloperInfo from "../components/profile/DeveloperInfo";
+
+export default {
+  name: "Profile",
+  components: {
+    DeveloperInfo
+  }
+};
 </script>
+
+<style lang="less" scoped>
+
+</style>
+
